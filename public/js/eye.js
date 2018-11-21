55081aea5faf36208b08163c9aa340cd9659b2cd
Evoxius <37443179+Evoxius@users.noreply.github.com>
2018-11-21 15:16:31 +0100

    p5 added to website

--- public/js/eye.js
+++ public/js/eye.js
@@ -1,32 +1,108 @@
 
-var mx = 1;
-var my = 1;
-var easing = 0.09;
-var radius = 24;
-var edge = 100;
-var inner = edge + radius;
-
-function setup() {
-  createCanvas(720, 400);
-  noStroke(); 
-  ellipseMode(RADIUS);
-  rectMode(CORNERS);
-}
-
-function draw() {
-  background(230);
   
-  if (abs(mouseX - mx) > 0.1) {
-    mx = mx + (mouseX - mx) * easing;
-  }
-  if (abs(mouseY - my) > 0.1) {
-    my = my + (mouseY- my) * easing;
+  // center point
+  var centerX = 0.0, centerY = 0.0;
+  
+  var radius = 45, rotAngle = -90;
+  var accelX = 0.0, accelY = 0.0;
+  var deltaX = 0.0, deltaY = 0.0;
+  var springing = 0.0009, damping = 0.98;
+  
+  //corner nodes
+  var nodes = 6;
+  
+  //zero fill arrays
+  var nodeStartX = [];
+  var nodeStartY = [];
+  var nodeX = [];
+  var nodeY = [];
+  var angle = [];
+  var frequency = [];
+  
+  // soft-body dynamics
+  var organicConstant = 1.0;
+  
+  function setup() {
+    var canvas = createCanvas(710, 400);
+  
+    //center shape in window
+    centerX = width/2;
+    centerY = height/2;
+  
+    //initialize arrays to 0
+    for (var i=0; i<nodes; i++){
+      nodeStartX[i] = 0;
+      nodeStartY[i] = 0;
+      nodeY[i] = 0;
+      nodeY[i] = 0;
+      angle[i] = 0;
+    }
+  
+    // iniitalize frequencies for corner nodes
+    for (var i=0; i<nodes; i++){
+      frequency[i] = random(5, 12);
+    }
+  
+    noStroke();
+    frameRate(30);
+    canvas.parent('sketch-holder');
   }
   
-  mx = constrain(mx, inner, width - inner);
-  my = constrain(my, inner, height - inner);
-  fill(237,34,93);
-  rect(edge, edge, width-edge, height-edge);
-  fill(255);
-  ellipse(mx, my, radius, radius);
-}
+  function draw() {
+    //fade background
+    fill(0, 100);
+    rect(0,0,width, height);
+    drawShape();
+    moveShape();
+  }
+  
+  function drawShape() {
+    //  calculate node  starting locations
+    for (var i=0; i<nodes; i++){
+      nodeStartX[i] = centerX+cos(radians(rotAngle))*radius;
+      nodeStartY[i] = centerY+sin(radians(rotAngle))*radius;
+      rotAngle += 360.0/nodes;
+    }
+  
+    // draw polygon
+    curveTightness(organicConstant);
+    fill(255);
+    beginShape();
+    for (var i=0; i<nodes; i++){
+      curveVertex(nodeX[i], nodeY[i]);
+    }
+    for (var i=0; i<nodes-1; i++){
+      curveVertex(nodeX[i], nodeY[i]);
+    }
+    endShape(CLOSE);
+  }
+  
+  function moveShape() {
+    //move center point
+    deltaX = mouseX-centerX;
+    deltaY = mouseY-centerY;
+  
+    // create springing effect
+    deltaX *= springing;
+    deltaY *= springing;
+    accelX += deltaX;
+    accelY += deltaY;
+  
+    // move predator's center
+    centerX += accelX;
+    centerY += accelY;
+  
+    // slow down springing
+    accelX *= damping;
+    accelY *= damping;
+  
+    // change curve tightness
+    organicConstant = 1-((abs(accelX)+abs(accelY))*0.1);
+  
+    //move nodes
+    for (var i=0; i<nodes; i++){
+      nodeX[i] = nodeStartX[i]+sin(radians(angle[i]))*(accelX*2);
+      nodeY[i] = nodeStartY[i]+sin(radians(angle[i]))*(accelY*2);
+      angle[i] += frequency[i];
+    }
+  }
